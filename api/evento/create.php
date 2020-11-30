685effcd6d0cfddeb3ecd993ed37ba9631e346e4
Wellington Favaron Chagas Mathias <wellington.mathias@gmail.com>
2020-11-30 00:36:57 -0300

    :bug: - Data Incomplete
    Erro na validação de dados incompletos

--- api/evento/create.php
+++ api/evento/create.php
@@ -23,11 +23,9 @@ $evento = new Evento($db);
 $data = json_decode(file_get_contents("php://input"));
   
 // make sure data is not empty
-$data_incomplete = !(empty($data->tipo_evento) && empty($data->dt_inicio) && empty($data->dt_fim) && empty($data->titulo) && empty($data->dia_letivo));
+$data_incomplete = empty($data->tipo_evento->id) && empty($data->dt_inicio) && empty($data->dt_fim) && empty($data->titulo) && empty($data->dia_letivo);
 
 if($data_incomplete) {
-    // tell the user data is incomplete
-  
     // set response code - 400 bad request
     http_response_code(400);
   
